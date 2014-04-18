f64026c77bb8bbf57848cf45a8f738211118d037
Nathan Shubert-Harbison <nathan@domain7.com>
2014-04-17 17:13:49 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# divideWidth
+
+divideWidth is a dead simple jQuery plugin for dividing elements evenly in the available space. It only really takes a line or two of JavaScript to achieve this, but putting it in a plugin can save doing it every time, especially when you need to kill the sizing for responsive sites at a certain breakpoint.
+
+## Use
+
+For simplest use, just call `.divideWidth()` on your containing element. The plugin looks for items with a class of `.item` but you can change it like such:
+
+```javascript
+$('#containing_element').divideWidth({
+  selector: '.custom_class'
+});
+```
+
+## Methods
+
+There are two methods for use in responsive sites that you can call with something like [Harvey.js](http://harvesthq.github.io/harvey/) or [enquire.js](http://wicky.nillia.ms/enquire.js/). They are `kill` to clear sizing, and `refresh` to put it back. The use case is entering and exiting a small breakpoint.
+
+Methods are called by passing them as follows:
+
+```javascript
+$('#containing_element').divideWidth('kill');
+```
---
+++ jquery.dividewidth.js
@@ -0,0 +1,66 @@
+/*!
+* divideWidth 1.0-beta1
+* 
+* Written by Nathan Shubert-Harbison for Domain7 (www.domain7.com) - #humanizetheweb
+* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
+* 
+*/
+
+(function($) {
+
+	var dw = {
+
+		defaults: {
+       selector: '.item'
+		},
+		settings: false,
+		items: false,
+
+		init: function(that) {
+		  return $(that).each(function(index) {		    
+		    dw.items = $(this).find(dw.settings.selector);
+		    dw.sizeItems();
+		  });
+		}, // init
+		
+		sizeItems: function() {
+		  dw.items.css('width', 100 / dw.items.length + "%");
+		}, // sizeItems
+		
+		methods: {
+
+		  refresh: function() {
+        dw.sizeItems();
+		  }, // refresh
+		  
+		  kill: function() {
+		    dw.items.css('width', '');
+		  } // kill
+		  
+		} // methods
+
+	}; // dw
+
+	$.fn.divideWidth = function(options) {
+
+			// Check if we're instantiating plugin with options or calling a method. Normal stuff first.
+			if ( !dw.methods[options] ) { 
+			
+				// Merge settings
+				dw.settings = $.extend(dw.defaults, options);
+
+				// Return main method
+				var output = dw.init(this);
+				return output;
+			
+			} else {
+
+				return this.each(function(index) {
+					dw.methods[options].apply(this, Array.prototype.slice.call($(this)));
+				});
+			}
+
+
+	}; // divideWidth
+
+}(jQuery));
